d9da7f2899a192500894ea359c213454f814ba68
white-duke3 <102415008+white-duke3@users.noreply.github.com>
2023-02-16 22:30:27 +0300

    sait

---
+++ controllers/openaiTextController.js
@@ -0,0 +1,52 @@
+const apiKey = require('../api.json')
+
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: apiKey.api,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const generateText = async (req, res) => {
+  const { prompt } = req.body;
+
+
+
+  try {
+   
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.5,
+      max_tokens: 1000,
+     
+      
+    });
+
+
+   // console.log(response.data.choices)
+
+    const answer = response.data.choices[0].text;
+
+    res.status(200).json({
+      success: true,
+      data: answer,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: 'The answer could not be generated',
+    });
+  }
+};
+
+module.exports = { generateText };
---
+++ readme.md
@@ -0,0 +1,24 @@
+# OpenAI ChatGPT Clone
+
+This is a demonestration of using Open AI API for text completion and image generation. The app is built with Node.js and Express that uses :
+- text-davinci-003 model
+- [OpenAI's Dall-E models](https://beta.openai.com/docs/guides/images).
+
+<img src="public/img/screen.png" width="500">
+
+## Usage
+
+Generate an API KEY at [OpenAI](https://beta.openai.com/) and add it to the `.env` file.
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+Run server
+
+```bash
+npm start
+```
+DEMO: [https://open-ai-chatgpt-clone.onrender.com/index.html](https://open-ai-chatgpt-clone.onrender.com/index.html)
